89a0bb8b9082742cdca724dd99e7bbdd2d78f62f
arpadav <arpadav@gmail.com>
2026-04-26 13:47:32 -0400

    new creplace

--- scripts/creplace.sh
+++ scripts/creplace.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Author: aav
 # --------------------------------------------------
 # Description:
@@ -11,14 +11,21 @@
 # quotes could exist and cause inconsistencies. This
 # script is mainly for aesthetics, and is not imperative
 # --------------------------------------------------
-# Usage: `bash scripts/creplace.sh`
+# Usage:
+#   `bash scripts/creplace.sh`
 # --------------------------------------------------
 set -euo pipefail
 
 # --------------------------------------------------
+# resolve this script's absolute path - this is to
+# exclude the same file
+# --------------------------------------------------
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+
+# --------------------------------------------------
 # file extensions to search for
 # --------------------------------------------------
-FILE_EXTENSIONS=("rs" "sh" "html" "md" "cu")
+FILE_EXTENSIONS=("rs" "sh" "html" "md" "cu" "js" "css" "md")
 name_expr=()
 for ext in "${FILE_EXTENSIONS[@]}"; do
     if [ ${#name_expr[@]} -gt 0 ]; then
@@ -28,15 +35,28 @@ for ext in "${FILE_EXTENSIONS[@]}"; do
 done
 
 # --------------------------------------------------
+# directories to exclude
+# --------------------------------------------------
+EXCLUDE_DIRS=("vendor" "target" ".git")
+prune_expr=()
+for dir in "${EXCLUDE_DIRS[@]}"; do
+    [ ${#prune_expr[@]} -gt 0 ] && prune_expr+=(-o)
+    prune_expr+=(-type d -name "$dir")
+done
+
+# --------------------------------------------------
 # * find file extensions in FILE_EXTENSIONS, recursively
 # * exclude `vendor/target` directories with a `Cargo.toml`
 # * perform the replacements using `sed`
 # --------------------------------------------------
-LC_ALL=en_US.UTF-8 find . -type f \( "${name_expr[@]}" \) \
+LC_ALL=en_US.UTF-8 find . \
+    \( "${prune_expr[@]}" \) -prune -o \
+    -type f \( "${name_expr[@]}" \) \
     ! -path "*/vendor/*" ! -path "*/target/*" \
+    ! -samefile "$SCRIPT_PATH" \
     -exec sed -i \
-    -e "s/'/'/g" \
-    -e "s/'/'/g" \
-    -e 's/[""]/"/g' \
+    -e "s/[’‘]/'/g" \
+    -e 's/[“”]/"/g' \
     -e $'s/\xE2\x80\x94/-/g' \
+    -e 's/→/->/g' \
     {} +
